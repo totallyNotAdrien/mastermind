c724fcbb5b88bfd7974fe89399209279e1ae6d0f
Tina Thompson <mark_junior13@yahoo.com>
2021-08-16 08:54:48 -0500

    added how to play

--- game.rb
+++ game.rb
@@ -9,6 +9,7 @@ class Game
     play_game = setup
 
     if play_game
+      how_to_play if @codebreaker.human
       puts "Crack the code in #{@max_attempts} attempts!"
 
       loop do
@@ -55,6 +56,9 @@ class Game
   end
 
   def setup
+    print "Welcome to Mastermind: a game where you can\n" +
+          "try to crack a 4-digit code of the computer's\n" +
+          "choosing, or vice versa\n\n"
     print "Select one of the following options:\n" +
             "\t[1] Crack the computer's code\n" +
             "\t[2] Have the computer crack your code\n" +
@@ -85,10 +89,40 @@ class Game
     if @curr_feedback == Codemaker::CORRECT * 4
       puts history_string
       puts "Conglaturation !!!\n#{congrat_str} cracked a great code."
-    elsif @attempt > 12
+    elsif @attempt > @max_attempts
       puts history_string
       puts codebreaker_str + " failed to crack the code."
     end
+  end
+
+  def how_to_play
+    puts
+    puts "How To Play"
+    puts
+    puts "[1]The computer will choose a 4-digit code using the digits 1-6."
+    puts "You must crack the code in #{@max_attempts} attempts."
+    puts
+    puts "[2]After making a guess, you will be shown your guess and feedback"
+    puts "about the accuracy of your guess using the letters 'C' and 'I'."
+    puts
+    puts "[3]The number of 'C's in the feedback is the number of correct"
+    puts "digits that are correctly placed in your guess."
+    puts
+    puts "[4]The number of 'I's is the number of correct digits that are"
+    puts "incorrectly placed in your guess."
+    puts
+    puts "[5]The order of the letters in the feedback will always have any 'C's"
+    puts "first, so there is no correlation between their order and your guess."
+    puts
+    puts "[6]Empty feedback means that none of the digits in your guess are correct."
+    puts "Feedback shorter than 4 characters means that there is at least one"
+    puts "digit in your guess that is not in the computer's code"
+    puts
+    puts "[7]Ex: a guess of '2546' and feedback of 'CCI' means that 2 digits"
+    puts "are exactly correct and 1 is incorrectly placed, NOT necessarily that"
+    puts "'2' and '5' are exactly correct and '4' is incorrectly placed"
+    puts
+    puts
   end
 
   attr_reader :codemaker
--- main.rb
+++ main.rb
@@ -3,17 +3,4 @@ require_relative 'codemaker.rb'
 require_relative 'codebreaker.rb'
 require_relative 'utils.rb'
 
-
-# until player quits
-#   show instructions
-#   ask if player will be maker, breaker, or neither, or quit
-#   create new game
-#   get code from maker
-#   until game finished
-#     get guess from breaker
-#     show guess feedback
-#   end
-#   show winner
-# end
-
 game = Game.new.play
